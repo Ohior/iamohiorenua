85a873f830a7f8b0311c4f5c23cde20afe6c076f
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2022-09-10 19:14:24 +0100

    Refactor

--- js/main.js
+++ js/main.js
@@ -115,23 +115,3 @@ jQuery(document).ready(function($) {
 
 
 });
-
-
-function sendEmail() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-    let subject = document.getElementById("subject").value;
-    let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Message : ${message} <br>`;
-    Email.send({
-        Host : "smtp.mailtrap.io",
-        Username : name,
-        Password : Ohior=1995,
-        To : 'aigbojeohiorenua@gmail.com',
-        From : email,
-        Subject : subject,
-        Body : finalmessage
-    }).then(
-      message => alert(message)
-    );
-}
